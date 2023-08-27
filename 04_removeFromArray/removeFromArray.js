31716a741a3f85708db3b384aaf9b77dfa494b78
Jacob Yoo <tolearneverything123@gmail.com>
2023-08-27 13:45:56 -0400

    Complete exercises up to 07_tempConversion

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...nums) {
+    let returnArr = arr;
+    for (let i=0; i<nums.length; i++) {
+        returnArr = returnArr.filter((element) => element !== nums[i]);
+    }
+    return returnArr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(start, stop) {
+    let sum = 0;
+    if (stop < 0 || start < 0 || typeof stop !== "number" || typeof start !== "number") {
+        return "ERROR";
+    }
+    if (stop < start) {
+        [stop, start] = [start, stop];
+    }
+    for (let i = start; i <= stop; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
